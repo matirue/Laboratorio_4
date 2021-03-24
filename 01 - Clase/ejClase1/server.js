8df88565f0b76c5dd9b0c1de4a87dc94bde083bf
matirue <matiasrue@gmail.com>
2021-03-24 20:34:05 -0300

    otro commit

--- 01 - Clase/ejClase1/server.js
+++ 01 - Clase/ejClase1/server.js
@@ -4,12 +4,12 @@ const app = express();
 
 const path = require('path');
 
-app.use(express.static(__dirname + '/angularapp'));
+app.use(express.static(__dirname + '/ej-clase1'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(reg, res) {
-    res.sendFile(path.join(__dirname + '/angularapp/index.html'));
+    res.sendFile(path.join(__dirname + '/src/index.html'));
 });
 
 console.log('Build successful!!');
